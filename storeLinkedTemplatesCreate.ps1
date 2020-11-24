34b8b73f34b7fd59459e7c760a874d254ba45137
Peter Geßler <peter.gessler@p3-group.com>
2020-11-24 10:52:29 +0100

    init push

---
+++ storeLinkedTemplatesCreate.ps1
@@ -0,0 +1,36 @@
+$projectName = Read-Host -Prompt "tkaadfdeuzccb72k6e"   # This name is used to generate names for Azure resources, such as storage account name.
+$location = Read-Host -Prompt "centralus"
+
+$resourceGroupName = $projectName + "rg"
+$storageAccountName = $projectName + "store"
+$containerName = "templates" # The name of the Blob container to be created.
+
+$linkedTemplateURL = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/get-started-deployment/linked-template/linkedStorageAccount.json" # A completed linked template used in this tutorial.
+$fileName = "linkedStorageAccount.json" # A file name used for downloading and uploading the linked template.
+
+# Download the template
+Invoke-WebRequest -Uri $linkedTemplateURL -OutFile "$home/$fileName"
+
+# Create a resource group
+#New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+$storageAccount = New-AzStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName "Standard_LRS"
+
+$context = $storageAccount.Context
+
+# Create a container
+New-AzStorageContainer -Name $containerName -Context $context -Permission Container
+
+# Upload the template
+Set-AzStorageBlobContent `
+    -Container $containerName `
+    -File "$home/$fileName" `
+    -Blob $fileName `
+    -Context $context
+
+Write-Host "Press [ENTER] to continue ..."
